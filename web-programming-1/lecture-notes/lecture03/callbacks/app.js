0e31874a0133fbf02a1608e43d1bb521dd917323
Mijeong Ban <mban1@stevens.edu>
2019-02-08 11:46:50 -0500

    code for reading, modify file added

--- web-programming-1/lecture-notes/lecture03/callbacks/app.js
+++ web-programming-1/lecture-notes/lecture03/callbacks/app.js
@@ -1,2 +1,44 @@
 const fs = require("fs");
 const prompt = require("prompt");
+
+const getfileOperation = {
+    name: "fileName",
+    description: "What file do you want to open?"
+};
+
+// The first asynchronous operation
+prompt.get([getfileOperation], function(err, result) {
+    console.log("Prompt has a result");
+    if (err) {
+        throw err;;
+    }
+
+    const fileName = result.fileName;
+    if(!fileName) {
+        throw "Need to provide a file name";
+    }
+
+    console.log(`About to read ${fileName} if it exists`);
+
+    // Read the file
+    fs.readFile(fileName, "uft-8", function(fileReadError, date) {
+        if(fileReadError) {
+            throw fileReadError;
+        }
+
+        // Modify 
+        const reversedContent = data.split("").reverse().join("");
+
+        // Save 
+        const reversedName = `reversed_${fileName}`;
+        fs.writeFile(reversedName, reversedContent, function(writeError) {
+            if(writeError) {
+                throw writeError;
+            }
+
+            console.log("Finished!");
+        });
+    });
+});
+
+console.log("After prompt is run");
