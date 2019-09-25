28c02b1727ebce0589e07e394d27017d76a4d2d3
Mijeong Ban <mban1@stevens.edu>
2019-09-25 19:03:07 -0400

    lecture 4

---
+++ web-programming-2/lecture-notes/lecture04/README.md
@@ -0,0 +1,5 @@
+# Lecture 04: React 2
+
+## Event Handling and Processing
+
+### React Event Handling
---
+++ web-programming-2/lecture-notes/lecture04/simple-component/src/components/Product.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class Product extends Component {
+  buttonClick = () => {
+    alert(this.props.product.productName + " was clicked");
+    this.props.handleClick(this.props.product.productName);
+  };
+  render() {
+    return (
+      <div>
+        <h1>{this.props.product.productName}</h1>
+        <p>{this.props.product.productDesc}</p>
+        <img src={process.env.PUBLIC_URL + this.props.product.imgName} />
+        <p>$ {this.props.product.productPrice}</p>
+        <br />
+        <button Onclick={this.buttonClick}>
+          Buy the {this.props.product.productName}
+        </button>
+        <br />
+        <br />
+      </div>
+    );
+  }
+}
+
+export default Product;
