34a28b4eb129cab769d1b4c8e86062255552bcde
Mijeong Ban <mban1@stevens.edu>
2019-09-27 14:41:06 -0400

    done with lab2

--- web-programming-2/labs/lab02/routes/index.js
+++ web-programming-2/labs/lab02/routes/index.js
@@ -1,8 +1,13 @@
 const path = require("path");
 
 const constructorMethod = app => {
-  app.use("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/index.html"));
+  app.use("/", (req, res, next) => {
+    /* Got help from Andrew for the part to deal with different path */
+    if (req.originalUrl !== "/") {
+      next();
+    } else {
+      res.sendFile(path.join(__dirname, "../views/index.html"));
+    }
   });
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Not found" });
